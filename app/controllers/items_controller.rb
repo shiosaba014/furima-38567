ecc0f87c32996de47d5032520c2d163227654dab
shiosaba014 <shiosaba014@gmail.com>
2022-10-28 13:59:02 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
   end
+
+  def new
+  end
+
+  def create
+  @user = User.find(params[:user_id])
+  @item = @user.items.new(item_params)
+  @item.save
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:explan,:category_id,:situation_id,:delivery_chage_id,:address_id,:period_id,:price,:image).merge(user_id: current_user.id)
+  end
+
 end
