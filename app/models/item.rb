82a2006e14088dee956d5300afc23367964b6236
shiosaba014 <shiosaba014@gmail.com>
2022-10-28 22:18:41 +0900

    商品登録可否の挙動の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+
+  validates :image,:name,:explan,:price, presence: true
   validates :category_id,:situation_id,:delivery_charge_id,:address_id,:period_id, numericality: { other_than:1 , message:"can't be blank"}
 end
