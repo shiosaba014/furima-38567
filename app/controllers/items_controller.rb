41f2bd0bb8d4167578e9b8cc178b3d549c6b8cac
shiosaba014 <shiosaba014@gmail.com>
2022-10-31 18:54:26 +0900

    削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.user_id == current_user.id
     @item.destroy
     redirect_to root_path
+    end
   end
 
   private
